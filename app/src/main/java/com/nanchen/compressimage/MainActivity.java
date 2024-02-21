d9cc149b9a51408b54c6f351f148decaaa594757
shixingxing <shixingxing@yungengxin.com>
2024-02-21 11:08:11 +0800

    androidX

--- app/src/main/java/com/nanchen/compressimage/MainActivity.java
+++ app/src/main/java/com/nanchen/compressimage/MainActivity.java
@@ -4,11 +4,12 @@ import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.nanchen.compresshelper.CompressHelper;
 import com.nanchen.compresshelper.FileUtil;
